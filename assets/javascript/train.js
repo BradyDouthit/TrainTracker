2d05937f52e70426f178f868aaaab056c5a8a4f1
Brady Douthit <brady.douthit99@gmail.com>
2019-06-27 12:03:15 -0400

    Connected the project to Firebase.

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,6 +1,29 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBCpS6D2JrxIdnkK4plncvNcERLt6KbLSc",
+    authDomain: "traintracker-edc8c.firebaseapp.com",
+    databaseURL: "https://traintracker-edc8c.firebaseio.com",
+    projectId: "traintracker-edc8c",
+    storageBucket: "",
+    messagingSenderId: "883902778956",
+    appId: "1:883902778956:web:3d2a3bbaa5a441b2"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
 $("#submit-button").on("click", function(event) {
     event.preventDefault();
+    var name = $("#train-name").val().trim();
+    var destination = $("#train-destination").val().trim();
+    var time = $("#train-time").val().trim();
+    var frequency = $("#train-frequency").val().trim();
     console.log("Submit ->");
-    console.log($("#train-name").val());
+    database.ref().set({
+        name: name,
+        destination: destination,
+        time: time,
+        frequency: frequency
+    });
     console.log("<- Submit");
 });
